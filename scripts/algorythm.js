dfff39d5822736a8f5dc0ec8cb08885015923321
PVL-dev <99485287+PVL-dev@users.noreply.github.com>
2022-09-13 13:50:33 +0200

    Algorithme version lente

--- scripts/algorythm.js
+++ scripts/algorythm.js
@@ -10,17 +10,32 @@ export const searchAlgorythm = (searchDatas) => {
         console.log("Aucune données à rechercher")
     } else {
         console.time('SearchTime');
-        searchDatas.forEach(data => {
-            let rawResult = sortedRecipes[0].filter(recette => {
-                return recette.name.toLowerCase().includes(data.toLowerCase())
-                    || recette.description.toLowerCase().includes(data.toLowerCase())
-                    || recette.appliance.toLowerCase().includes(data.toLowerCase())
-                    || recette.ingredients.some((ingredient)=> ingredient.ingredient.toLowerCase().includes(data.toLowerCase()))
-                    || recette.ustensils.some((ustensil)=> ustensil.toLowerCase().includes(data.toLowerCase()));
-            });
-            console.log(rawResult);
+        for(let l1 = 0; l1 < searchDatas.length; l1++) {
+            // Pour chaque valeur à rechercher dans les recettes
+            const data = searchDatas[l1].toLowerCase();
+            console.log(data);
+            let rawResult = [];
+
+            for(let l2 = 0; l2 < sortedRecipes[0].length; l2++) {
+                // Pour chaque recettes dans l'Array sortedRecipes
+                const recette = sortedRecipes[0][l2];
+                console.log(recette);
+                
+                if (recette.name.toLowerCase().includes(data) 
+                || recette.description.toLowerCase().includes(data)
+                || recette.appliance.toLowerCase().includes(data)
+                || recette.ingredients.some((ingredient)=> ingredient.ingredient.toLowerCase().includes(data))
+                || recette.ustensils.some((ustensil)=> ustensil.toLowerCase().includes(data))) {
+                    rawResult.push(recette);
+                    console.log("Oui");
+                } else {
+                    console.log("Non");
+                };
+                
+                console.log(rawResult);
+            };
             sortedRecipes[0] = rawResult;
-        });
+        };
         console.timeEnd('SearchTime');
     };
     

b919512af1f155e85313d1d46c041f90086a857b
PVL-dev <99485287+PVL-dev@users.noreply.github.com>
2022-09-16 02:15:10 +0200

    Finalisation projet

--- scripts/algorythm.js
+++ scripts/algorythm.js
@@ -13,26 +13,20 @@ export const searchAlgorythm = (searchDatas) => {
         for(let l1 = 0; l1 < searchDatas.length; l1++) {
             // Pour chaque valeur à rechercher dans les recettes
             const data = searchDatas[l1].toLowerCase();
-            console.log(data);
             let rawResult = [];
 
             for(let l2 = 0; l2 < sortedRecipes[0].length; l2++) {
                 // Pour chaque recettes dans l'Array sortedRecipes
                 const recette = sortedRecipes[0][l2];
-                console.log(recette);
                 
-                if (recette.name.toLowerCase().includes(data) 
+                if (recette.name.toLowerCase().includes(data)
                 || recette.description.toLowerCase().includes(data)
                 || recette.appliance.toLowerCase().includes(data)
                 || recette.ingredients.some((ingredient)=> ingredient.ingredient.toLowerCase().includes(data))
                 || recette.ustensils.some((ustensil)=> ustensil.toLowerCase().includes(data))) {
                     rawResult.push(recette);
-                    console.log("Oui");
-                } else {
-                    console.log("Non");
                 };
                 
-                console.log(rawResult);
             };
             sortedRecipes[0] = rawResult;
         };
